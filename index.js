c021ad6c359025bab67d404b799ebe2d0a60c045
NipunHarshana0 <herokunipun123@gmail.com>
2025-01-19 15:01:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -319,6 +319,22 @@ await conn.groupParticipantsUpdate(from,[sender], 'remove')
 
   }
  }
+
+if(config.STATUS_REACT === "true" ) {      
+if (!mek.message) return        
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+ if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+                let emoji = [
+                    '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎',
+                ];
+                let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+                await conn.readMessages([mek.key]);
+                conn.sendMessage(
+                    'status@broadcast',
+                    { react: { text: sigma, key: mek.key } },
+                    { statusJidList: [mek.key.participant] },
+                );
+ }
   
 //============================================================================
 
