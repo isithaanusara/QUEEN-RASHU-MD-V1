f25fa5797df4520440665b956e6a63c16f44d6be
NipunHarshana0 <herokunipun123@gmail.com>
2025-01-20 02:36:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -120,8 +120,24 @@ await conn.readMessages([mek.key]); // Mark the message as read but don't send d
 mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-await conn.readMessages([mek.key])
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true") {
+        await conn.readMessages([mek.key]);
+    }
+
+    if (config.AUTO_REACT_STATUS === "true") {
+        if (!mek.message) return;
+
+        mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message;
+        if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+            let emoji = ['😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '💗', '✅', '☘️', '👋', '😁', '☠️', '💩', '🧚‍♂️', '👣'];
+            let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+            await conn.readMessages([mek.key]);
+            conn.sendMessage(
+                'status@broadcast',
+                { react: { text: sigma, key: mek.key } },
+                { statusJidList: [mek.key.participant] }
+            );
+        }
 }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
