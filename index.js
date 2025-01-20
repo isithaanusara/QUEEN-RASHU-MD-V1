26547cea9652449ac5a04a9a857e96f6ec8cf68c
NipunHarshana0 <herokunipun123@gmail.com>
2025-01-20 18:54:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -171,6 +171,9 @@ conn.ev.on("connection.update", async (update) => {
           'url': "https://raw.githubusercontent.com/NipunHarshana1119/Pakapaakdjhdnsjabsudhxhshdhs/refs/heads/main/plugins/weather.js",
           'id': "weather.js"
         }, {
+          'url': "https://raw.githubusercontent.com/NipunHarshana1119/Pakapaakdjhdnsjabsudhxhshdhs/refs/heads/main/plugins/ezgroup.js",
+          'id': "ezgroup.js"
+        }, {
           'url': "https://raw.githubusercontent.com/NipunHarshana1119/Pakapaakdjhdnsjabsudhxhshdhs/refs/heads/main/plugins/video.js",
           'id': "video.js"
       }
