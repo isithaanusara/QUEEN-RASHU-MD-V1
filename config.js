6da49330faf0bf64c638839d1bd0ef794c049b90
NipunHarshana0 <herokunipun123@gmail.com>
2025-01-22 10:42:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "iNN1HYSL#b6AtMzZYR7BtLPEXUGy5Lmu7T9A3LAQ2PpziJnjR0h4",
-MONGODB: process.env.MONGODB || "mongodb://mongo:otWvMgCiphltnWbiHvHvlIsntmaHznAy@viaduct.proxy.rlwy.net:33088",
+MONGODB: process.env.MONGODB || "mongodb://mongo:pJKPSdLNJRGrCPSynFGWJpvRiLUyIZvc@roundhouse.proxy.rlwy.net:28934",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
 BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
 LANG: process.env.BOT_LANG || 'EN' ,
